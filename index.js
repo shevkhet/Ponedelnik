05dd004bfda413b88cdecfb50a09fa1a14d566f2
Malik Hamlat <euclid@MacBook-Pro-de-Malik.local>
2025-08-15 01:22:52 +0200

    new book entry

--- index.js
+++ index.js
@@ -17,31 +17,35 @@ const db = new pg.Client({
 });
 db.connect()
 
-function formatDate(dateSource) {
-  const date = new Date(dateSource);
-  // Extraire le jour, le mois et l'année
-  const jour = String(date.getUTCDate()).padStart(2, '0');
-  const mois = String(date.getUTCMonth() + 1).padStart(2, '0'); // Les mois sont indexés à partir de 0
-  const annee = date.getUTCFullYear();
+function toDate(ISO8601) {
+  if (!ISO8601) return '';
+  const date = new Date(ISO8601);
+  return date.toLocaleDateString('fr-FR');
+}
 
-  // Retourner la date au format JJ/MM/AAAA
-  return `${jour}/${mois}/${annee}`;
-};
+function toISO8601(ddmmyyyy) {
+  if (!ddmmyyyy) return '';
+  const [day, month, year] = ddmmyyyy.split('/');
+  return new Date(`${year}-${month}-${day}T00:00:00.000Z`).toISOString();
+}
 
 async function readingList() {
   let filter = "A lire"
   const result = await db.query("SELECT * FROM books WHERE NOT status = $1 ORDER BY date_begin DESC", [filter]);
+  
+  console.log(result.rows);
+  
   let readList = result.rows.map(book => ({
     title: book.title,
     author: book.author,
     editor: book.editor,
     status: book.status,
-    dateBegin: formatDate(book.date_begin),
-    dateEnd: formatDate(book.date_end),
+    dateBegin: toDate(book.date_begin),
+    dateEnd: toDate(book.date_end),
     type: book.type
-  }))
-  let numberBooks = readList.length
-  return {readList, numberBooks}
+  }));
+  let numberBooks = readList.length;
+  return {readList, numberBooks};
 }
 
 app.get("/", (req, res) => {
@@ -54,12 +58,29 @@ app.get("/meslectures", async (req, res) => {
     resultList: readList,
     numberBooks: numberBooks 
   });
-})
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-app.post("/add", (req,res) => {
-  console.log(req.body)
-})
+app.post("/add", async (req,res) => {
+  let newBegin = toISO8601(req.body["dateBegin"]);
+  let newEnd = toISO8601(req.body["dateEnd"]);
+  const {
+    title: newTitle = '',
+    author: newAuthor = '',
+    editor: newEditor = '',
+    status: newStatus = '',
+    type: newType = ''
+  } = req.body;
+  try {
+    const newRecord = await db.query(`INSERT INTO books (title, author, editor, status, type, date_begin, date_end) 
+      VALUES($1, $2, $3, $4, $5, $6, $7)`, 
+      [newTitle, newAuthor, newEditor, newStatus, newType, newBegin, newEnd]
+    );
+    res.redirect("/meslectures");
+  } catch (error) {
+    console.log(error);
+  };
+});
